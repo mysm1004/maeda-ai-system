5bd1314f54efc75c44460eee62b50950453036dc
mysm1004 <mysm1004@users.noreply.github.com>
2026-03-09 12:27:59 +0000

    auto-sync: server.js, src/db/schema.js, src/server.js +1件

--- server.js
+++ server.js
@@ -336,6 +336,7 @@ async function processLineCommand(text, userId) {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   // ============ モード切替 ============
   if (t === 'PCモード' || t === 'pcモード' || t === 'PC' || t === 'ローカル') {
     try {
@@ -395,6 +396,8 @@ async function processLineCommand(text, userId) {
     return msg2;
   }
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
